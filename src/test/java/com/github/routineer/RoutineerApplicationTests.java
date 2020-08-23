208687adbe04eea609a2f8bb8a1e227b1a4ef6ad
Victor Gomov <chellitorvic@gmail.com>
2020-08-23 17:25:58 +0300

    Init commit 🎉

---
+++ src/test/java/com/github/routineer/RoutineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.routineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
